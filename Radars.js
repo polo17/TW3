41300e24f8098f8bee05abe31ef88d6c5e6b44b7
Ludo81 <36764393+Ludo81@users.noreply.github.com>
2019-03-21 09:14:45 +0100

    Radars

---
+++ Radars.js
@@ -0,0 +1,61 @@
+{ 'radars': [
+{
+"nom" : "Nord-Ouest vers Sud-Est",
+"date" : "2012-09-20",
+"type" : "Franchissement de feux",
+"vitesse" : "-1",
+"coordonees" : ["43.58181","3.86399"],
+"departement" : "34",
+"ville" : "MONTPELLIER",
+"rue" : "-"
+},
+{
+"nom" : "CARMAUX VERS ALBI",
+"date" : "2005-04-26",
+"type" : "Vitesse VL 80",
+"vitesse" : "80",
+"coordonees" : ["43.99942","2.16705"],
+"departement" : "81",
+"ville" : "LE GARRIC",
+"rue" : "RN2088"
+},
+{
+"nom" : "LAVAUR vers CASTRES",
+"date" : "2005-02-28",
+"type" : "Vitesse VL 80",
+"vitesse" : "80",
+"coordonees" : ["43.68225","1.85319"],
+"departement" : "81",
+"ville" : "MASSAC SERAN",
+"rue" : "RD112"
+},
+{
+"nom" : "BEAUCAIRE vers NIMES",
+"date" : "2013-04-05",
+"type" : "Vitesse VL 90",
+"vitesse" : "90",
+"coordonees" : ["43.8356","4.4026"],
+"departement" : "30",
+"ville" : "NIMES",
+"rue" : "RD999"
+},
+{
+"nom" : "FOIX vers TOULOUSE",
+"date" : "2014-03-14",
+"type" : "Vitesse VL 130",
+"vitesse" : "130",
+"coordonees" : ["43.38309","1.6163"],
+"departement" : "31",
+"ville" : "MONTESQUIEU LAURAGAIS",
+"rue" : "A66"
+},
+{
+"nom" : "Seysses vers Toulouse",
+"date" : "2011-03-18",
+"type" : "Franchissement de feux",
+"vitesse" : "-1",
+"coordonees" : ["43.55761","1.40172"],
+"departement" : "31",
+"ville" : "TOULOUSE",
+"rue" : "-"
+},
